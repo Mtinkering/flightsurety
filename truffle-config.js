c69b3413ee8737f5d2ae29ef1a3ad1589796d957
Steven Nguyen <steven.nguyenhung@gmail.com>
2021-08-10 01:36:23 +0800

    Set up app locally

---
+++ truffle-config.js
@@ -0,0 +1,25 @@
+// const HDWalletProvider = require("@truffle/hdwallet-provider");
+// var mnemonic =
+//   "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+
+module.exports = {
+  networks: {
+    development: {
+      // provider: function () {
+      //   return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+      // },
+      network_id: "*",
+      gas: 9999999,
+    },
+    develop: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+    },
+  },
+  compilers: {
+    solc: {
+      version: "^0.4.24",
+    },
+  },
+};
