183f9125505ee754405a422689c45c718c57e4fd
Steven Nguyen <steven.nguyenhung@gmail.com>
2021-08-14 15:16:18 +0800

    Working on registration for airline {

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,23 +1,34 @@
 const FlightSuretyApp = artifacts.require("FlightSuretyApp");
 const FlightSuretyData = artifacts.require("FlightSuretyData");
-const fs = require('fs');
+const fs = require("fs");
 
-module.exports = function(deployer) {
+module.exports = async function (deployer) {
+  let firstAirline = "0xf17f52151EbEF6C7334FAD080c5704D77216b732";
 
-    let firstAirline = '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
-    deployer.deploy(FlightSuretyData)
-    .then(() => {
-        return deployer.deploy(FlightSuretyApp)
-                .then(() => {
-                    let config = {
-                        localhost: {
-                            url: 'http://localhost:8545',
-                            dataAddress: FlightSuretyData.address,
-                            appAddress: FlightSuretyApp.address
-                        }
-                    }
-                    fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                    fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                });
-    });
-}
+  await deployer.deploy(FlightSuretyData);
+  await deployer.deploy(FlightSuretyApp);
+
+  const [data, app] = await Promise.all(
+    [FlightSuretyData, FlightSuretyApp].map((instance) => instance.deployed())
+  );
+
+  let config = {
+    localhost: {
+      url: "http://localhost:8545",
+      dataAddress: data.address,
+      appAddress: app.address,
+    },
+  };
+  fs.writeFileSync(
+    __dirname + "/../src/dapp/config.json",
+    JSON.stringify(config, null, "\t"),
+    "utf-8"
+  );
+  fs.writeFileSync(
+    __dirname + "/../src/server/config.json",
+    JSON.stringify(config, null, "\t"),
+    "utf-8"
+  );
+
+  await app.registerAirline(firstAirline);
+};
